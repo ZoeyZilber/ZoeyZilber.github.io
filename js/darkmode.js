ae475bda8efc417273dec86b59750836066359f3
ZZilber <zoeyzilber@gmail.com>
2024-12-26 19:40:27 -0500

    Update darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,3 +1,12 @@
 function toggleDarkMode() {
-    document.body.classList.toggle('darkmode');
+    const isDarkMode = document.body.classList.toggle('darkmode');
+
+    localStorage.setItem('darkmode', isDarkMode ? 'enabled' : 'disabled');
 }
+
+window.onload = function () {
+    const darkModeSetting = localStorage.getItem('darkmode');
+    if (darkModeSetting === 'enabled') {
+        document.body.classList.add('darkmode');
+    }
+};
